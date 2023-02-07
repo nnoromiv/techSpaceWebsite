f1a660b6ba4d62d2174a16a8074272c2e19a7215
Nnorom-coder <nnorom.prince44@gmail.com>
2023-02-07 07:57:18 +0100

    Content Updated 0.1

--- src/pages/MyCarousel.jsx
+++ src/pages/MyCarousel.jsx
@@ -2,23 +2,33 @@ import React from 'react'
 import { Carousel } from 'react-bootstrap'
 import "./styles/MyCarousel.scss"
 
-const TESTIMG = 'https://image-processor-storage.s3.us-west-2.amazonaws.com/images/3cf61c1011912a2173ea4dfa260f1108/halo-of-neon-ring-illuminated-in-the-stunning-landscape-of-yosemite.jpg'
+const CAROUSELIMAGE = [
+    {
+        key: 'first-slide',
+        image: 'https://image-processor-storage.s3.us-west-2.amazonaws.com/images/3cf61c1011912a2173ea4dfa260f1108/halo-of-neon-ring-illuminated-in-the-stunning-landscape-of-yosemite.jpg'
+    },
+    {
+        key: 'second-slide',
+        image: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+    },
+    {
+        key: 'third-slide',
+        image: 'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'
+    }
+]
 
 function MyCarousel() {
   return (
     <Carousel className='Carousel' fade>
-        <Carousel.Item>
-            <img alt='first-slide' src={TESTIMG} srcSet='' />
-        </Carousel.Item>
-        <Carousel.Item>
-            <img alt='first-slide' src={TESTIMG} srcSet='' />
-        </Carousel.Item>
-        <Carousel.Item>
-            <img alt='first-slide' src={TESTIMG} srcSet='' />
-        </Carousel.Item>
-        <Carousel.Item>
-            <img alt='first-slide' src={TESTIMG} srcSet='' />
-        </Carousel.Item>
+    {
+        CAROUSELIMAGE.map(carouselimage => {
+            return(
+                <Carousel.Item key={carouselimage.key}>
+                    <img alt={carouselimage.key} src={carouselimage.image} srcSet='' />
+                </Carousel.Item>
+            )
+        })
+    }
     </Carousel>
   )
 }
