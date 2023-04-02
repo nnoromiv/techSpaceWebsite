f43d2dbbd5d7735ef8ce05f37b7a9ac07b00d56c
Nnorom-coder <nnorom.prince44@gmail.com>
2023-04-02 12:01:16 +0100

    Blog & FAQ

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./styles/Blog.scss"
 
 const BLOGCARD = [
@@ -29,25 +32,69 @@ const BLOGCARD = [
     }
 ]
 
+
 function Cards(){
-    return(
+    var SLIDERSETTINGS = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        draggable: true,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2,
+                autoplaySpeed: 3000,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    }
+    return(        
         <div className='Card'>
-        {
-            BLOGCARD.map(blogcard => {
-                return (
-                    <Card key={blogcard.title}>
-                        <Card.Img variant='top' src={blogcard.image}/>
-                        <Card.Body>
-                            <Card.Subtitle>{blogcard.date}</Card.Subtitle>
-                            <Card.Title>{blogcard.title}</Card.Title>
-                            <Card.Text>{blogcard.shortText}</Card.Text>
-                            <Button variant='outline-primary'>Read more</Button>
-                        </Card.Body>
-                    </Card>
-                )
-            })
-        }
+            <Slider {...SLIDERSETTINGS}>
+                {
+                    BLOGCARD.map(blogcard => {
+                        return (
+
+                                <Card key={blogcard.title}>
+                                    <Card.Img variant='top' src={blogcard.image}/>
+                                    <Card.Body>
+                                        <Card.Subtitle>{blogcard.date}</Card.Subtitle>
+                                        <Card.Title>{blogcard.title}</Card.Title>
+                                        <Card.Text>{blogcard.shortText}</Card.Text>
+                                        <Button variant='outline-primary'>Read more</Button>
+                                    </Card.Body>
+                                </Card>
+
+                        )
+                    })
+                }
+            </Slider>
         </div>
+        
     )
 }
 
